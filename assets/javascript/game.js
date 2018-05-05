75c13bc1593a28f493e8180e6563c23c154791ac
Maniacal Laura <maniacallaura@gmail.com>
2018-05-05 13:44:36 -0600

    changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -32,7 +32,7 @@ window.onload = function() {
         for (var i = 0; i < breeds[ordIndex].length; i++) {
             actualWord.push("_");
         }
-        // Hide game over and win images/text
+
         document.getElementById("winImageLab").style.cssText= "display: none";
         document.getElementById("winImageWiem").style.cssText = "display: none";
         document.getElementById("winImageBeag").style.cssText = "display: none";
@@ -42,9 +42,9 @@ window.onload = function() {
         document.getElementById("winImageDach").style.cssText = "display: none";
         
 
-        // Show display
+        
         updateDisplay();
-};
+        }
         function updateDisplay() {
 
             document.getElementById("wins").innerText = wins;
@@ -58,71 +58,34 @@ window.onload = function() {
                 document.getElementById("gameover-image").style.cssText = "display: block";
                 document.getElementById("pressKeyTryAgain").style.cssText = "display:block";
                 hasFinished = true;
-    }
-};
-// Updates the image depending on how many guesses
+            }
+        }
         function updateHangmanImage() {
             document.getElementById("title-image").src = "assets/images/HangmanTitle.png" + (maxTries - guessesRemaining) + ".png";
-};
+            }
 
             document.onkeydown = function(event) {
-                // If we finished a game, dump one keystroke and reset.
                 if(hasFinished) {
                     resetGame();
                     hasFinished = false;
                 } else {
-                    // Check to make sure a-z was pressed.
+                
                     if(event.keyCode >= 65 && event.keyCode <= 90) {
                         makeGuess(event.key.toLowerCase());
                     }
-                }
-            };
+                }   
+            }
         function makeGuess(letter) {
             if (guessesRemaining > 0) {
                 if (!gameStarted) {
                     gameStarted = true;
                 }
         
-                // Make sure we didn't use this letter yet
+                
                 if (lettersGuessed.indexOf(letter) === -1) {
                     lettersGuessed.push(letter);
                     evaluateGuess(letter);
                 }
             }
-            
-            updateDisplay();
-            checkWin();
-        };
-        
-        function evaluateGuess(letter) {
-            // Array to store positions of letters in string
-            var positions = [];
-        
-            // Loop through word finding all instances of guessed letter, store the indicies in an array.
-            for (var i = 0; i < breeds[currentWordIndex].length; i++) {
-                if(breeds[wordIndex][i] === letter) {
-                    positions.push(i);
-                }
-            }
-        
-            // if there are no indicies, remove a guess and update the hangman image
-            if (positions.length <= 0) {
-                guessesRemaining--;
-                updateHangmanImage();
-            } else {
-                // Loop through all the indicies and replace the '_' with a letter.
-                for(var i = 0; i < positions.length; i++) {
-                    guessingWord[positions[i]] = letter;
-                }
-            }
-            function checkWin() {
-                if(guessingWord.indexOf("_") === -1) {
-                    document.getElementById("youwin-image").style.cssText = "display: block";
-                    document.getElementById("pressKeyTryAgain").style.cssText= "display: block";
-                    wins++;
-                    hasFinished = true;
-                }
-            };
-        };
-        
-}
+        }
+    }
